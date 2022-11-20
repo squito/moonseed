ccce332b84839fbee749e7bcee7017c0ae246ef1
Imran Rashid <irashid@cloudera.com>
2022-11-19 20:57:05 -0600

    working code to sync filters and tests

--- filters.py
+++ filters.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 import rest
 
 
@@ -15,15 +18,36 @@ class Filters:
   def update_filter(self, filter_dict):
     """Given the json of the filter, create the filter (if it doesn't have an id),
        or update the existing filter.  Returns the full json (as a dict) of the filter 
-       (with an id).  The passed in json only needs name and jql.
+       (with an id).  The passed in json only needs name and jql.  It may also have a
+       "comment" field, which will be stripped before posting, but retained in return
+       value.
     """
+    comment_to_add = None
+    if "comment" in filter_dict:
+      filter_to_upload = filter_dict.copy()
+      comment_to_add = filter_to_upload.pop("comment", None)
+    else:
+      filter_to_upload = filter_dict
     if "id" in filter_dict:
       # PUT an update for the filter
       fid = filter_dict["id"]
-      return self._client._put(f"/api/2/filter/{fid}", body=filter_dict)
+      rv = self._client._put(f"/api/2/filter/{fid}", body=filter_to_upload)
     else:
       # POST a new filter.  important to return the result, as that is the only
       # way to capture the ID of the created filter
-      return self._client._post(f"/api/2/filter", body=filter_dict)
+      rv = self._client._post(f"/api/2/filter", body=filter_to_upload)
+    if comment_to_add is not None:
+      rv["comment"] = comment_to_add
+    return rv
 
-      
+
+  def sync_filters(self, filter_dir):
+    files = os.listdir(filter_dir)
+    for f in files:
+      full_path = filter_dir + "/" + f
+      with open(full_path) as in_f:
+        loaded_filter = json.load(in_f)
+        new_filter = self.update_filter(loaded_filter)
+      with open(full_path, "w") as out_f:
+        json.dump(new_filter, out_f)
+
--- test/test_filters.py
+++ test/test_filters.py
@@ -1,7 +1,77 @@
+import json
 import mock
+import os
+import shutil
 import unittest
+
+import filters 
 
 class TestFilters(unittest.TestCase):
 
+  def test_update_filter(self):
+    mock_client = mock.Mock()
+    f = filters.Filters(mock_client)
+    # comment gets stripped before posted, but retained in returned value
+    no_id = {"name": "Blah", "jql":"foo", "comment": "ignore me"}
+    exp = no_id.copy()
+    exp.pop("comment", None)
+    mock_client._post.return_value = exp.copy()
+    new_filter = f.update_filter(no_id)
+    mock_client._post.assert_called_with("/api/2/filter", body=exp)
+    self.assertIn("comment", new_filter)
+    self.assertEqual("ignore me", new_filter["comment"])
+
+    with_id = no_id
+    with_id["id"] = "12345"
+    exp = with_id.copy()
+    exp.pop("comment", None)
+    mock_client._put.return_value = exp.copy()
+    updated_filter = f.update_filter(with_id)
+    mock_client._put.assert_called_with("/api/2/filter/12345", body=exp)
+    self.assertIn("comment", new_filter)
+    self.assertEqual("ignore me", new_filter["comment"])
+
+
+
+  resource_dir = "test/resources/"
+  input_filter_dir = resource_dir + "input_filters/"
+  resulting_filter_dir = resource_dir + "resulting_filters/"
+  working_filter_dir = resource_dir + "working_dir/"
+
   def test_sync_filters(self):
-    pass
+    shutil.rmtree(self.working_filter_dir)
+    shutil.copytree(self.input_filter_dir, self.working_filter_dir)
+    file_to_orig_json = {}
+    exp = {}
+    for f in os.listdir(self.working_filter_dir):
+      with open(self.working_filter_dir + f) as f_in:
+        file_to_orig_json[f] = json.load(f_in)
+        exp[f] = file_to_orig_json[f].copy()
+        exp[f]["blah"] = "foo"
+        
+    mock_client = mock.Mock()
+    # TODO this should actually be something updated w/ an ID
+    mock_client._post.return_value = exp["simple_1.json"]
+    mock_client._put.return_value = exp["simple_2.json"]
+
+    f = filters.Filters(mock_client)
+
+    f.sync_filters(self.working_filter_dir)
+
+    print(file_to_orig_json)
+
+    mock_client._put.assert_called_with(
+      "/api/2/filter/1",
+      body=file_to_orig_json["simple_2.json"])
+    mock_client._post.assert_called_with(
+      "/api/2/filter",
+      body=file_to_orig_json["simple_1.json"])
+
+    # Make sure the results got updated with what actually came back from
+    # server (in this case, I just put in a "blah" entry)
+    for f in os.listdir(self.working_filter_dir):
+      with open(self.working_filter_dir + f) as f_in:
+        actual = json.load(f_in)
+        self.assertEqual(exp[f], actual)
+
+
