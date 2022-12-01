e0e196556a7310320351a4de8f3df8f952813bec
Imran Rashid <irashid@cloudera.com>
2022-11-30 23:03:40 -0600

    Filters mirrored to a specific dir, tracks files in it.  Tests added & passing

--- filters.py
+++ filters.py
@@ -8,17 +8,42 @@ class Filters:
 
   # https://docs.atlassian.com/software/jira/docs/api/REST/8.22.1/#filter
 
-  def __init__(self, jira_rest_client):
+  def __init__(self, jira_rest_client, filter_dir):
     self._client = jira_rest_client
+    self._filter_dir = filter_dir
+    if not os.path.exists(filter_dir):
+      os.makedirs(filter_dir)
+    self._reload_id_to_filter()
 
   def my_filters(self):
     return self._client._get("/api/2/filter/favourite")
 
+
+  def get_filter(self, fid):
+    return self._client._get(f"/api/2/filter/{fid}")
+
   
-  def update_filter(self, filter_dict):
-    """Given the json of the filter, create the filter (if it doesn't have an id),
+  def _reload_id_to_filter(self):
+    self._id_to_filter = {}
+    self._id_to_file = {}
+    if not os.path.exists(self._filter_dir):
+      return
+    for f in os.listdir(self._filter_dir):
+      full_path = self._filter_dir + f
+      with open(full_path) as f_in:
+        loaded = json.load(f_in)
+        if "id" in loaded:
+          fid = loaded["id"]
+          self._id_to_filter[fid] = loaded
+          self._id_to_file[fid] = full_path
+        else:
+          print("WARN: filter file " + full_path + " does not have an id yet")
+
+  
+  def update_filter(self, filter_dict, orig_file=None):
+    """Given the filter (as a dict), create the filter (if it doesn't have an id),
        or update the existing filter.  Returns the full json (as a dict) of the filter 
-       (with an id).  The passed in json only needs name and jql.  It may also have a
+       (with an id).  The passed in dict only needs name and jql.  It may also have a
        "comment" field, which will be stripped before posting, but retained in return
        value.
     """
@@ -36,18 +61,59 @@ class Filters:
       # POST a new filter.  important to return the result, as that is the only
       # way to capture the ID of the created filter
       rv = self._client._post(f"/api/2/filter", body=filter_to_upload)
+      fid = rv["id"]
+      self._id_to_filter[fid] = rv
     if comment_to_add is not None:
       rv["comment"] = comment_to_add
+
+    # write the filter back to a file
+    if fid in self._id_to_file:
+      out_file = self._id_to_file[fid]
+    elif orig_file is not None:
+      out_file = orig_file
+    else: 
+      out_file = self.__suggested_filter_filename(fid, rv["name"])
+    with open(out_file, "w") as out_w:
+      json.dump(rv, out_w, indent=2)
+    self._id_to_file[fid] = out_file
+
     return rv
 
 
-  def sync_filters(self, filter_dir):
+  def __suggested_filter_filename(self, filter_id, filter_name):
+    return self._filter_dir + f"/{filter_name}_{filter_id}.json"
+
+
+  def sync_filters_from_dir(self, filter_dir):
+    """Sync filters between a directory and the jira server.
+       Filters already in the dir will be uploaded to the server -- they can be
+       completely new filters, or just updates to existing filters.
+
+       All existing files in the dir must be json files representing jira filters."""
+
     files = os.listdir(filter_dir)
     for f in files:
-      full_path = filter_dir + "/" + f
+      full_path = os.path.normpath(filter_dir + "/" + f)
       with open(full_path) as in_f:
         loaded_filter = json.load(in_f)
-        new_filter = self.update_filter(loaded_filter)
+        new_filter = self.update_filter(loaded_filter, orig_file = full_path)
       with open(full_path, "w") as out_f:
         json.dump(new_filter, out_f)
+
+
+  def sync_filters_from_server(self, filter_ids):
+    """Download the given set of filters from server, and store to files in directory.
+       If the filters exist in the dir already, they will be updated.  Otherwise, new
+       files will be created (based on filter name + id).
+
+       Any changes to the local files will be overwritten -- recommended to only use
+       this if you've committed local changes to git.
+
+       If there is a comment in the local file, it will be retained.
+       """
+    for f in os.listdir(self._filter_dir):
+      with open(self._filter_dir + f) as f_in:
+        actual = json.load(f_in)
+        self.assertEqual(exp[f], actual)
+
 
--- test/test_filters.py
+++ test/test_filters.py
@@ -8,14 +8,29 @@ import filters
 
 class TestFilters(unittest.TestCase):
 
+  resource_dir = "test/resources/"
+  input_filter_dir = resource_dir + "input_filters/"
+  resulting_filter_dir = resource_dir + "resulting_filters/"
+  working_filter_dir = resource_dir + "working_dir/"
+
+  def setUp(self):
+    if os.path.exists(self.working_filter_dir):
+      shutil.rmtree(self.working_filter_dir)
+
+
   def test_update_filter(self):
     mock_client = mock.Mock()
-    f = filters.Filters(mock_client)
+    f = filters.Filters(mock_client, self.working_filter_dir)
+    self.assertEqual(f._id_to_filter, {})
+    self.assertEqual(f._id_to_file, {})
     # comment gets stripped before posted, but retained in returned value
     no_id = {"name": "Blah", "jql":"foo", "comment": "ignore me"}
     exp = no_id.copy()
     exp.pop("comment", None)
-    mock_client._post.return_value = exp.copy()
+    # jira server should always add an ID, and we'll record that
+    rv = exp.copy()
+    rv["id"] = "567"
+    mock_client._post.return_value = rv.copy()
     new_filter = f.update_filter(no_id)
     mock_client._post.assert_called_with("/api/2/filter", body=exp)
     self.assertIn("comment", new_filter)
@@ -30,16 +45,13 @@ class TestFilters(unittest.TestCase):
     mock_client._put.assert_called_with("/api/2/filter/12345", body=exp)
     self.assertIn("comment", new_filter)
     self.assertEqual("ignore me", new_filter["comment"])
+    self.assertIn("567", f._id_to_filter)
+    self.assertIn("567", f._id_to_file)
 
 
 
-  resource_dir = "test/resources/"
-  input_filter_dir = resource_dir + "input_filters/"
-  resulting_filter_dir = resource_dir + "resulting_filters/"
-  working_filter_dir = resource_dir + "working_dir/"
 
-  def test_sync_filters(self):
-    shutil.rmtree(self.working_filter_dir)
+  def test_sync_filters_from_dir(self):
     shutil.copytree(self.input_filter_dir, self.working_filter_dir)
     file_to_orig_json = {}
     exp = {}
@@ -50,15 +62,16 @@ class TestFilters(unittest.TestCase):
         exp[f]["blah"] = "foo"
         
     mock_client = mock.Mock()
-    # TODO this should actually be something updated w/ an ID
+    # the value returned from the server should *always* contain an id for the filter.
+    # in this test, we want to make sure that some internal bookkeeping happens w/ that
+    # id.
+    exp["simple_1.json"]["id"] = "123"
     mock_client._post.return_value = exp["simple_1.json"]
     mock_client._put.return_value = exp["simple_2.json"]
 
-    f = filters.Filters(mock_client)
+    fs = filters.Filters(mock_client, self.working_filter_dir)
 
-    f.sync_filters(self.working_filter_dir)
-
-    print(file_to_orig_json)
+    fs.sync_filters_from_dir(self.working_filter_dir)
 
     mock_client._put.assert_called_with(
       "/api/2/filter/1",
@@ -74,4 +87,23 @@ class TestFilters(unittest.TestCase):
         actual = json.load(f_in)
         self.assertEqual(exp[f], actual)
 
+    # make sure that we updated the internal id_to_... dicts w/ the new id
+    self.assertIn("123", fs._id_to_file)
+    self.assertEqual(fs._filter_dir + "simple_1.json", fs._id_to_file["123"])
+    self.assertEqual(exp["simple_1.json"], fs._id_to_filter["123"])
 
+  def test_sync_filters_from_server(self):
+    mock_client = mock.Mock()
+    f = filters.Filters(mock_client, self.working_filter_dir)
+
+    id_to_return_value = {
+      "1": {"name": "Blah", "jql":"foo", "id": "1"},
+      "2": {"name": "Flippy", "jql":"zim", "id": "2"}
+    }
+    def rv(passed_id):
+      return id_to_return_value[passed_id]
+    mock_client._get.side_effect = rv
+
+    f.sync_filters_from_server
+
+    pass
